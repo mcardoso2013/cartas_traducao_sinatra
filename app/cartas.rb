8230933d86070a845651f9089024cb7a554adae2
Henrique Morato <riquemorato@gmail.com>
2020-03-06 22:15:48 -0300

    minha pagina

--- app/cartas.rb
+++ app/cartas.rb
@@ -5,4 +5,12 @@ set :static, true
 Tilt.register Tilt::ERBTemplate, 'html.erb'
 
 get '/' do
+  @cartas = []
+  @cartas << Carta.new(ingles: 'Red', portugues: 'Vermelho')
+  @cartas << Carta.new(ingles: 'Blue', portugues: 'Azul')
+  @cartas << Carta.new(ingles: 'Yellow', portugues: 'Amarelo')
+  @cartas << Carta.new(ingles: 'Black', portugues: 'Preto')
+  @cartas << Carta.new(ingles: 'Pink', portugues: 'Rosa')
+  @cartas << Carta.new(ingles: 'Orange', portugues: 'Laranja')
+  erb :index
 end
